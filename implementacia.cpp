bd3ef4f9952c2df7c1a9d31607eb70f95cdd5c57
ArtLeg <art.legeida@gmail.com>
2023-06-16 11:02:47 +0200

    prerobil a dorobil som hlavnu funkciu

--- implementacia.cpp
+++ implementacia.cpp
@@ -25,6 +25,16 @@ NUMAP* numap_create_empty()
     return(a);
 }
 
+//SEQ* seq_create()
+//{
+//    SEQ* a = (SEQ*)malloc(sizeof(NUMAP));
+//    a->map = NULL;
+//    a->size_cod = 0;
+//    a->size_d = 0;
+//    a->is_perm = 0;
+//    return(a);
+//}
+
 void numap_destroy(NUMAP* to_destroy)
 {
     free(to_destroy->map);
@@ -58,25 +68,70 @@ void numap_print(NUMAP* f)
 {
     for (int i = 0; i < f->size_d; i++)
     {
-        printf("%d %d\n", i, f->map[i]);
+        printf("%d ", f->map[i]);
     }
+    printf("\n");
+}
+
+bool search(int* a, int x, int size)
+{
+    int f = 0;
+    int l = size - 1;
+    while (f <= l) {
+        int mid = l + (f - l) / 2;
+        if (x == a[mid])
+            return true;
+        if (x > a[mid])
+            f = mid + 1;
+        if (x < a[mid])
+            l = mid - 1;
+    }
+    return false;
 }
 
 char numap_rand_perm_cycle_type(NUMAP* to_redef, SEQ* cycle_type)
 {
     for (int i = 0; i < cycle_type->size; i++) {
         int temp = cycle_type->seq[i];
+        int* a = (int*)malloc(sizeof(int) * temp);
+        int size = 0;
         for (int j = 0; j < temp; j++) {
-            int a = rand() % to_redef->size_d;
-            int b = rand() % to_redef->size_d;
-            while (b == a) {
-                b = rand() % to_redef->size_d;
+            int x = rand() % (to_redef->size_d);
+            //printf("%d ", x);
+            if (j == 0) {
+                a[0] = x;
+                size++;
+                continue;
             }
-            int t = to_redef->map[a];
-            to_redef->map[a] = to_redef->map[b];
-            to_redef->map[b] = t;
-            numap_print(to_redef);
+            while (true) {
+                if (search(a, x, size))
+                    x = rand() % (to_redef->size_d);
+                else
+                    break;
+                
+            }
+            a[j] = x;
+            size++;
+            //numap_print(to_redef);
         }
+        //printf("\n");
+        int* b = (int*)malloc(sizeof(int) * temp);
+        for (int j = 0; j < temp; j++) {
+            if ((j + 1) != size) {
+                b[j] = to_redef->map[a[j + 1]];
+            }
+            else {
+                b[j] = to_redef->map[a[0]];
+            }
+        }
+        //for (int j = 0; j < temp; j++) {
+        //    printf("%d ", b[j]);
+        //}
+        //printf("\n");
+        for (int j = 0; j < temp; j++) {
+            to_redef->map[a[j]] = b[j];
+        }
+        numap_print(to_redef);
     }
     return 0;
 }
@@ -93,12 +148,13 @@ int main()
     //numap_print(test);
     test = create_zob(test, size);
     numap_print(test);
-    numap_destroy(test);
+    //numap_destroy(test);
     SEQ* a = (SEQ*)malloc(sizeof(SEQ));
     a->size = 1;
     a->seq = (unsigned int*)malloc(sizeof(int) * a->size);
-    a->seq[0] = 1;
-    printf("%d\n", a->seq[0]);
+    a->seq[0] = 4;
+    //printf("%d\n", a->seq[0]);
+    //printf("size = %d\n", test->size_d);
     numap_rand_perm_cycle_type(test, a);
     numap_print(test);
 }
