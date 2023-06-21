84467526263a30fc8e8d4719bdbebdbfc2d2d06c
ArtLeg <art.legeida@gmail.com>
2023-06-21 11:44:43 +0200

    create numap.h a propisal som to co tam musilo by byt

---
+++ numap.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+typedef struct {
+    unsigned int size_d;
+    unsigned int size_cod;
+    char is_perm;
+    int* map;
+}NUMAP;
+
+typedef struct {
+    unsigned int size;
+    unsigned int* seq;
+}SEQ;
+
+NUMAP* numap_create_empty();
+
+void numap_destroy(NUMAP* to_destroy);
+
+NUMAP* numap_id(NUMAP* to_redef, unsigned int size);
+
+NUMAP* create_zob(NUMAP* f, unsigned int size);
+
+void numap_print(NUMAP* f);
+
+bool search(int* a, int x, int size);
+
+char numap_rand_perm_cycle_type(NUMAP* to_redef, SEQ* cycle_type);
